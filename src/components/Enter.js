ee527f90c31b03f9bd44f0d597d073ced9d6c139
georgebsmith-tech <georgebsmith.tech@gmail.com>
2022-05-31 14:03:03 +0100

    made a correction

--- src/components/Enter.js
+++ src/components/Enter.js
@@ -1,11 +1,24 @@
 import React from "react";
+import Switch from "@mui/material/Switch";
 
 export default function Enter() {
+  const [isOn, setIsOn] = React.useState(false);
+  let cont = "";
+  if (isOn) {
+    cont = <p>it is on</p>;
+  }
   return (
     <section className="summ">
       <section className="much">
         <h3>Enter Amount</h3>
         <p>How much do you want to deposit ?</p>
+        {/* {cont} */}
+        {/* {isOn && <p>it is on</p>} */}
+        {isOn ? (
+          <p style={{ height: 30 }}>it is on</p>
+        ) : (
+          <p style={{ height: 30 }}> </p>
+        )}
 
         <section className="convertHolder">
           <section className="convert">
@@ -22,12 +35,13 @@ export default function Enter() {
             </div>
           </section>
           <section className="calc">
-            <section className="in">
-              <div className="control">
-                <div className="circle">
-                  <img src="/turn.png"></img>
-                </div>
-              </div>
+            <section className="in flex align-center">
+              {/* <div className="control"> */}
+              {/* <div className="circle"> */}
+              {/* <img src="/turn.png"></img> */}
+              <Switch checked={isOn} onChange={() => setIsOn(!isOn)} />
+              {/* </div> */}
+              {/* </div> */}
               <p>Include fees into calculation</p>
             </section>
           </section>
